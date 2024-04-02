9b30a35fdbf5c01f0c9222f188a243f184ebf342
Leandro Dias <lvd@certi.org.br>
2024-04-02 17:22:22 -0300

    start project

---
+++ src/main/java/com/senai/projetonotas/repository/AlunoRepository.java
@@ -0,0 +1,7 @@
+package com.senai.projetonotas.repository;
+
+import com.senai.projetonotas.entity.AlunoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<AlunoEntity, Long> {
+}
---
+++ src/main/java/com/senai/projetonotas/repository/MatriculaRepository.java
@@ -0,0 +1,7 @@
+package com.senai.projetonotas.repository;
+
+import com.senai.projetonotas.entity.MatriculaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<MatriculaEntity, Long> {
+}
