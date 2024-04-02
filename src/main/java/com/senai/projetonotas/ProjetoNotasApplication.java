4ed327cdb9ad4bb8f45e59581d2051fc47e0257e
Suene Souza <suene.vs@gmail.com>
2024-04-01 21:28:41 -0300

    First Commit

---
+++ src/main/java/com/senai/projetonotas/ProjetoNotasApplication.java
@@ -0,0 +1,13 @@
+package com.senai.projetonotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNotasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoNotasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senai/projetonotas/ProjetoNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.projetonotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNotasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
