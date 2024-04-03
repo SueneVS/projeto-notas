85829ac8c1a703a444e44028cb058d8f80bef53d
Leandro Dias <lvd@certi.org.br>
2024-04-03 19:31:41 -0300

    file update

--- src/main/java/com/senai/projetonotas/repository/AlunoRepository.java
+++ src/main/java/com/senai/projetonotas/repository/AlunoRepository.java
@@ -3,5 +3,6 @@ package com.senai.projetonotas.repository;
 import com.senai.projetonotas.entity.AlunoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AlunoRepository extends JpaRepository<AlunoEntity, Long> {
 }
--- src/main/java/com/senai/projetonotas/repository/ProfessorRepository.java
+++ src/main/java/com/senai/projetonotas/repository/ProfessorRepository.java
@@ -3,5 +3,6 @@ package com.senai.projetonotas.repository;
 import com.senai.projetonotas.entity.ProfessorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ProfessorRepository extends JpaRepository<ProfessorEntity, Long> {
 }
