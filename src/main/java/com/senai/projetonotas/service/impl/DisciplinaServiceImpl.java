5b0a5eab198679c81cb442a41313cf3473008be0
Scheila Stihler <devscheila@gmail.com>
2024-04-07 11:39:11 -0300

    Update DisciplinaServiceImpl.java
    Reinserida linhas excluídas por engano

--- src/main/java/com/senai/projetonotas/service/impl/DisciplinaServiceImpl.java
+++ src/main/java/com/senai/projetonotas/service/impl/DisciplinaServiceImpl.java
@@ -1,7 +1,10 @@
 package com.senai.projetonotas.service.impl;
 import com.senai.projetonotas.entity.DisciplinaEntity;
 import com.senai.projetonotas.entity.MatriculaEntity;
+import com.senai.projetonotas.exception.customException.CampoObrigatorioException;
+import com.senai.projetonotas.exception.customException.NotFoundException;
 import com.senai.projetonotas.repository.DisciplinaRepository;
+import com.senai.projetonotas.repository.ProfessorRepository;
 import com.senai.projetonotas.service.DisciplinaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,40 +16,48 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DisciplinaServiceImpl implements DisciplinaService {
 
-    private final DisciplinaRepository repository;
-    //private final ProfessorRepository Prespository;
+    private final DisciplinaRepository Drepository;
+    private final ProfessorRepository Prespository;
 
     @Override
     public DisciplinaEntity create(DisciplinaEntity dto) {
-        //Prespository.findById(dto.getProfessor().getProfessorId()).orElseThrow(() -> new RuntimeException("Error"));
-        return repository.save(dto);
+
+        if (dto.getNome() == null || dto.getProfessor() == null  ) {
+            throw new CampoObrigatorioException("Os campos 'nome' e 'professorId' são obrigatório para criar uma disciplina");
+        }
+        Prespository.findById(dto.getProfessor().getProfessorId()).orElseThrow(() -> new NotFoundException("Não encontrado professor com este id"));
+
+        return Drepository.save(dto);
     }
 
     @Override
     public void delete(Long id) {
         getEntity(id);
-        repository.deleteById(id);
+        Drepository.deleteById(id);
     }
 
     @Override
     public DisciplinaEntity update(Long id, DisciplinaEntity dto) {
         getEntity(id);
+        if (dto.getNome() == null || dto.getProfessor() == null  ) {
+            throw new CampoObrigatorioException("Os campos 'nome' e 'professorId' são obrigatório ao atualizar uma disciplina");
+        }
+        Prespository.findById(dto.getProfessor().getProfessorId()).orElseThrow(() -> new NotFoundException("Não encontrado professor com este id"));
         dto.setDisciplinaId(id);
-        return repository.saveAndFlush(dto);
+        return Drepository.saveAndFlush(dto);
     }
     public DisciplinaEntity getEntity(Long id) {
-        return repository.findById(id).orElseThrow(() -> new RuntimeException("Error"));
+        return Drepository.findById(id).orElseThrow(() -> new NotFoundException("Disciplina não encontrada com o id: " + id));
     }
 
     @Override
     public List<DisciplinaEntity> getEntities() {
-        return repository.findAll();
+        return Drepository.findAll();
     }
 
 
     @Override
     public List<MatriculaEntity> getEntitiesProfessor(Long id) {
-        return repository.findAllByProfessorProfessorId(id);
+        return Drepository.findAllByProfessorProfessorId(id);
     }
 }
-
